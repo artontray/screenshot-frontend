7e17ccab690a60bc2f37705797b5c9e570b09b84
Damien <db.webapp@gmail.com>
2023-03-12 12:39:53 +0000

    Searching bar on Listing screenshot working

--- src/pages/scrshot/ListScrshotPublicPage.js
+++ src/pages/scrshot/ListScrshotPublicPage.js
@@ -7,44 +7,70 @@ import Container from "react-bootstrap/Container";
 
 import ScrshotPublic from "./ScrshotPublic";
 import Asset from "../../components/Asset";
-
 import appStyles from "../../App.module.css";
 import styles from "../../styles/ListScrshotPublicPage.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 import NoResults from "../../assets/no-results.png";
 
 function ListScrshotPublicPage({ message, filter = "" }) {
-    const [scrshot, setScrshots] = useState({ results: [] });
+    const [scrshots, setScrshots] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
   
+    const [query, setQuery] = useState("");
+    
     useEffect(() => {
       const fetchScrshots = async () => {
         try {
-          const { data } = await axiosReq.get(`/public-scrshot/?${filter}`);
+          const { data } = await axiosReq.get(`/public-scrshot/?${filter}search=${query}`);
           setScrshots(data);
           setHasLoaded(true);
         } catch (err) {
           console.log(err);
         }
       };
-  
       setHasLoaded(false);
-      fetchScrshots();
-    }, [filter, pathname]);
+      const timer = setTimeout(() => {
+        fetchScrshots();
+      }, 1000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [filter, query, pathname]);
   
     return (
-      <Row className="h-100">
+        <Row className="h-100">
         <Col className="py-2 p-0 p-lg-2" lg={8}>
           <p>Popular profiles mobile</p>
+          <i className={`fas fa-search ${styles.SearchIcon}`} />
+          <Form
+            className={styles.SearchBar}
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <Form.Control
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              type="text"
+              className="mr-sm-2"
+              placeholder="Search Screenshots"
+            />
+          </Form>
+  
           {hasLoaded ? (
             <>
-              {scrshot.results.length ? (
-                scrshot.results.map((scrshot) => (
-                  <ScrshotPublic key={scrshot.id} {...scrshot} setScrshots={setScrshots} />
-                ))
+              {scrshots.results.length ? (
+                <InfiniteScroll
+                  children={scrshots.results.map((scrshot) => (
+                    <ScrshotPublic key={scrshot.id} {...scrshot} setScrshots={setScrshots} />
+                  ))}
+                  dataLength={scrshots.results.length}
+                  loader={<Asset spinner />}
+                  hasMore={!!scrshots.next}
+                  next={() => fetchMoreData(scrshots, setScrshots)}
+                />
               ) : (
                 <Container className={appStyles.Content}>
                   <Asset src={NoResults} message={message} />
